99e03e553c81fe4d8d070ffdeb4104a2510d9e44
KaniaruIvy <69512969+KaniaruIvy@users.noreply.github.com>
2024-03-06 13:44:26 +0000

    Value section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero';
+import Companies from './components/Companies/Companies';
+import Residencies from './components/Residencies/Residencies';
+import Value from './components/Value/Value';
+import './App.css';
 function App() {
   return (
-    <div className="app">
-      <Header />
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
     </div>
   );
 }
