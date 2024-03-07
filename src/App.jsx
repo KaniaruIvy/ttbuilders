ab15a3e6489db27c9aef0aee8d889ed1c6c43047
KaniaruIvy <69512969+KaniaruIvy@users.noreply.github.com>
2024-03-07 06:31:51 +0000

    Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Residencies from './components/Residencies/Residencies';
 import Value from './components/Value/Value';
 import Contact from './components/Contact/Contact';
 import GetStarted from './components/GetStarted/GetStarted';
+import Footer from './components/Footer/Footer';
 import './App.css';
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Value />
       <Contact />
       <GetStarted />
+      <Footer />
     </div>
   );
 }
