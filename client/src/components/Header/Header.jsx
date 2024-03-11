c520807695eb32a6c68cb8d5f075b8e3a87ce298
KaniaruIvy <69512969+KaniaruIvy@users.noreply.github.com>
2024-03-11 17:21:11 +0000

    Fetch properties from db and dispay on properties page and spinners

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {BiMenuAltRight} from 'react-icons/bi';
+import {NavLink,Link} from "react-router-dom";
 import OutsideClickHandler from "react-outside-click-handler";
 import './Header.css'
 
@@ -13,7 +14,10 @@ const Header = () => {
     return(
         <section className="h-wrapper">
             <div className="flexCenter paddings innerWidth h-container">
-                <img src="./logo.png" alt="logo" width={100} />
+
+                <Link to="/">
+                    <img src="./logo.png" alt="logo" width={100} />
+                </Link>
 
                 <OutsideClickHandler
                 onOutsideClick={()=>{
@@ -23,13 +27,14 @@ const Header = () => {
                 <div className="flexCenter h-menu"
                 style={getMenuStyles(menuOpened)}
                 >
-                    <a href="">Residencies</a>
-                    <a href="">Our Values</a>
-                    <a href="">Contact Us</a>
-                    <a href="">Get Started</a>
+                    <NavLink to="/properties">Properties</NavLink>
+                    <a href="mailto:test@gmail.com">Contact</a>
+
+                    {/* Login Button */}
                     <button className="button">
-                        <a href="">Contact</a>
+                        Login
                     </button>
+                    
                 </div>
                 </OutsideClickHandler>
                 
